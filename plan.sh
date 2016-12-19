562cf6367afa677edbb2450f2d717c6d70805412
Tim Johnson <timothy.johnson2@purina.nestle.com>
2016-12-19 10:13:34 -0500

    add gcc-libs to dependency

--- plan.sh
+++ plan.sh
@@ -8,11 +8,10 @@ pkg_source=http://downloads.sourceforge.net/project/mcrypt/Libmcrypt/${pkg_versi
 pkg_shasum=bf2f1671f44af88e66477db0982d5ecb5116a5c767b0a0d68acb34499d41b793
 pkg_filename=${pkg_distname}-${pkg_version}.tar.bz2
 pkg_dirname=${pkg_distname}-${pkg_version}
-pkg_deps=(core/zlib)
+pkg_deps=(core/gcc-libs)
 pkg_build_deps=(core/gcc core/make)
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
 pkg_interpreters=()
-
 
